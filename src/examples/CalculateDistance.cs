1a6412bd625012621ed52b21e2365ed403379b57
Marijn van der Zee <marijn@serraict.com>
2013-08-29 14:30:00 +0200

    Add shortest path example

---
+++ src/examples/CalculateDistance.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using QuickGraph;
+using QuickGraph.Algorithms;
+using QuickGraph.Graphviz;
+using examples.Support;
+
+namespace examples
+{
+    [TestFixture]
+    public class CalculateDistance
+    {
+        private AdjacencyGraph<string, Edge<string>> _graph;
+        private Dictionary<Edge<string>, double> _costs;
+
+        [SetUp]
+        public void SetUpEdgesAndCosts()
+        {
+            _graph = new AdjacencyGraph<string, Edge<string>>();
+            _costs = new Dictionary<Edge<string>, double>();
+   
+            AddEdgeWithCosts("A", "D", 4.0);
+            AddEdgeWithCosts("D", "E", 3.0);
+            AddEdgeWithCosts("D", "B", 1.0);
+            AddEdgeWithCosts("B", "D", 1.0);
+            AddEdgeWithCosts("B", "E", 1.0);
+            AddEdgeWithCosts("E", "A", 6.0);
+            AddEdgeWithCosts("C", "E", 4.0);
+            AddEdgeWithCosts("C", "B", 1.0);
+        }
+
+        private void AddEdgeWithCosts(string source, string target, double cost)
+        {
+            var edge = new Edge<string>(source, target);
+            _graph.AddVerticesAndEdge(edge);
+            _costs.Add(edge, cost);
+        }
+
+        void CostEdgeFormatter(object sender, FormatEdgeEventArgs<string, Edge<string>> e)
+        {
+            e.EdgeFormatter.Label.Value = _costs[e.Edge].ToString("0.0");
+        }
+
+        [Test]
+        public void Visualize()
+        {
+            Console.WriteLine(_graph.ToDotNotation());
+            _graph.Visualize("distance", CostEdgeFormatter);
+        }
+
+        [Test]
+        public void ShortesDistanceTest()
+        {
+            PrintShortestPath("A", "E");
+        }
+
+        private void PrintShortestPath(string @from, string to)
+        {
+            var edgeCost = AlgorithmExtensions.GetIndexer(_costs);
+            var tryGetPath = _graph.ShortestPathsDijkstra(edgeCost, @from);
+
+            IEnumerable<Edge<string>> path;
+            if (tryGetPath(to, out path))
+            {
+                Console.Write("Path found from {0} to {1}: {0}", @from, to);
+                foreach (var e in path)
+                    Console.Write(string.Format(" > {0}", e.Target));
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("No path found from {0} to {1}.");
+            }
+        }
+    }
+}
---
+++ src/examples/Support/Visualizer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using QuickGraph;
+using QuickGraph.Graphviz;
+using QuickGraph.Graphviz.Dot;
+
+namespace examples.Support
+{
+    internal static class Visualizer
+    {
+        public static void Visualize<TVertex, TEdge>(this IVertexAndEdgeListGraph<TVertex, TEdge> graph,
+            string fileName, string dir = @"c:\temp\")
+            where TEdge : IEdge<TVertex>
+        {
+            Visualize(graph, fileName, NoOpEdgeFormatter, dir);
+        }        
+        
+        public static void Visualize<TVertex, TEdge>(this IVertexAndEdgeListGraph<TVertex, TEdge> graph,
+            string fileName, FormatEdgeAction<TVertex, TEdge> edgeFormatter, string dir = @"c:\temp\")
+            where TEdge : IEdge<TVertex>
+        {
+            var fullFileName = Path.Combine(dir, fileName);
+            var viz = new GraphvizAlgorithm<TVertex, TEdge>(graph);
+
+            viz.FormatVertex += VizFormatVertex;
+
+            viz.FormatEdge += edgeFormatter;
+
+            viz.Generate(new FileDotEngine(), fullFileName);
+        }
+        
+        static void NoOpEdgeFormatter<TVertex, TEdge>(object sender, FormatEdgeEventArgs<TVertex, TEdge> e)
+            where TEdge : IEdge<TVertex>
+        {
+            // noop
+        }
+        
+        public static string ToDotNotation<TVertex, TEdge>(this IVertexAndEdgeListGraph<TVertex, TEdge> graph)
+            where TEdge : IEdge<TVertex>
+        {
+            var viz = new GraphvizAlgorithm<TVertex, TEdge>(graph);
+            viz.FormatVertex += VizFormatVertex;
+            return viz.Generate(new DotPrinter(), "");
+        }
+
+        static void VizFormatVertex<TVertex>(object sender, FormatVertexEventArgs<TVertex> e)
+        {
+            e.VertexFormatter.Label = e.Vertex.ToString();
+        }
+    }
+
+    public sealed class FileDotEngine : IDotEngine
+    {
+        public string Run(GraphvizImageType imageType, string dot, string outputFileName)
+        {
+            string output = outputFileName;
+            File.WriteAllText(output, dot);
+
+            // assumes dot.exe is on the path:
+            var args = string.Format(@"{0} -Tjpg c:\temp\products.dot -O", output);
+            System.Diagnostics.Process.Start("dot.exe", args);
+            return output;
+        }
+    }
+        
+    public sealed class DotPrinter : IDotEngine
+    {
+        public string Run(GraphvizImageType imageType, string dot, string outputFileName)
+        {
+            return dot;
+        }
+    }
+}
